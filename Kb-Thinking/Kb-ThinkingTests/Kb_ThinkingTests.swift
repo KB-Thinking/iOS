4af8c1c4cdcff6045e4f022c532aed418b72e76c
정정욱 <jeonguk29@naver.com>
2025-08-07 15:38:58 +0900

    feat: 초기 프로젝트 셋팅

---
+++ Kb-Thinking/Kb-ThinkingTests/Kb_ThinkingTests.swift
@@ -0,0 +1,16 @@
+//
+//  Kb_ThinkingTests.swift
+//  Kb-ThinkingTests
+//
+//  Created by jeonguk29 on 8/6/25.
+//
+
+import Testing
+
+struct Kb_ThinkingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
