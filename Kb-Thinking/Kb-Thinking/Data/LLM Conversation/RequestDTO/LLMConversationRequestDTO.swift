5d605b6f4444eb6d1b0a775e603f73e92be3f1a7
정정욱 <jeonguk29@naver.com>
2025-08-08 17:37:28 +0900

    feat: LLM 대화 관련 Data 영역 생성 (DTO, RepositoryImpl) 및 목업 객체 생성

---
+++ Kb-Thinking/Kb-Thinking/Data/LLM Conversation/RequestDTO/LLMConversationRequestDTO.swift
@@ -0,0 +1,12 @@
+//
+//  LLMConversationRequestDTO.swift
+//  Kb-Thinking
+//
+//  Created by jeonguk29 on 8/8/25.
+//
+
+import Foundation
+
+struct LLMConversationRequestDTO: Encodable {
+    let requestText: String
+}
