fb418b9b026f960530cf5316dd82f69d8d1b7c46
정정욱 <jeonguk29@naver.com>
2025-08-11 16:04:27 +0900

    feat: Coordinator 패턴 적용

---
+++ Kb-Thinking/Kb-Thinking/Domain/Route.swift
@@ -0,0 +1,13 @@
+//
+//  Route.swift
+//  Kb-Thinking
+//
+//  Created by jeonguk29 on 8/11/25.
+//
+
+import Foundation
+
+public enum Route: String, Decodable {
+    case limit = "LIMIT"
+    // 서버와 합의된 값 추가
+}
